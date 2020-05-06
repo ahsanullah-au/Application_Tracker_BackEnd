570fc942b47d0a0a15d23e07815163c5db4aafd6
Ahsan Ullah <ahsanullah.au@gmail.com>
2020-05-06 19:58:37 -0400

    Updated register to include first and last names

--- controllers/register.js
+++ controllers/register.js
@@ -1,7 +1,7 @@
 const handleRegister = (req, res, db, bcrypt) => {
 
     
-    if (!req.body.email || !req.body.name || !req.body.password) {
+    if (!req.body.email || !req.body.lastname || !req.body.firstname || !req.body.password) {
         return res.status(400).json('Missing Info')
     }
 
@@ -19,7 +19,8 @@ const handleRegister = (req, res, db, bcrypt) => {
                     return trx('users')
                         .returning('*')
                         .insert({
-                            firstname: req.body.name,
+                            firstname: req.body.firstname,
+                            lastname: req.body.lastname,
                             email: loginEmail[0]
                         }).then(response => {
                             res.json(response[0])
