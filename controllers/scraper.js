994758c7bc6473c445821b2a543224cb17951289
Ahsan Ullah <ahsanullah.au@gmail.com>
2020-05-28 11:59:41 -0400

    Scraper now saves to a html for further analysis

--- controllers/scraper.js
+++ controllers/scraper.js
@@ -1,16 +1,23 @@
-const handleScraper = () => {
+const handleScraper = (url) => {
     const rp = require('request-promise');
-    const url = "https://ca.indeed.com/rc/clk?jk=9627bc5f7fd04c72&amp;from=vj&amp;pos=twoPaneCopyLink"
+    const fs = require('fs');
+
+    
 
     rp(url).then((res) => {
-        console.log(res)
+        fs.writeFileSync(__dirname+ "/tmp/html2.html", res)
     })
     
 
 
 }
 
-handleScraper();
+//let url = "https://ca.indeed.com/rc/clk?jk=9627bc5f7fd04c72&amp;from=vj&amp;pos=twoPaneCopyLink"
+
+let url = "https://ca.indeed.com/rc/clk?jk=af8e98d91ec68409&amp;from=vj&amp;ia=1&amp;sjdu=vQIlM60yK_PwYat7ToXhk_BtMgCfoRREjwFTpnTZpxDr2nMIVM2LzYtTq8PoTuWCyVgYsB-CrVACzuliRquR0KdVsNzT9tVrFUobQiL-3go&amp;spon=1&amp;adid=347462548&amp;pos=twoPaneCopyLink"
+
+handleScraper(url);
+
 
 //Job Location, City. 
 //Role
