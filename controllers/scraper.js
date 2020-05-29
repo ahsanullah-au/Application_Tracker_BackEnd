f5dc3d74f1e1b95ed019a9a5848c7ec34dfbda0e
Ahsan Ullah <ahsanullah.au@gmail.com>
2020-05-29 12:04:07 -0400

    Linkedin and Indeed Scraper on server

--- controllers/scraper.js
+++ controllers/scraper.js
@@ -1,17 +1,46 @@
 const handleScraper = (req, res, rp, fs, HTMLParser) => {
-    rp(req.body.URL)
-        .then((resp) => {
-            //fs.writeFileSync(__dirname + "/tmp/html3.html", res)
-            const htmlResponse = HTMLParser.parse(resp).querySelector('#indeedApplyButtonContainer').firstChild
-            return ({
-                jobTitle: htmlResponse.getAttribute("data-indeed-apply-jobTitle"),
-                jobLocation: htmlResponse.getAttribute("data-indeed-apply-jobLocation"),
-                jobCompanyName: htmlResponse.getAttribute("data-indeed-apply-jobCompanyName"),
-                jobURL: htmlResponse.getAttribute("data-indeed-apply-jobUrl")
-            })
+    if (req.body.site === "Indeed") {
+        rp(req.body.URL)
+            .then((resp) => {
+                const htmlResponse = HTMLParser.parse(resp).querySelector('#indeedApplyButtonContainer').firstChild
+                return ({
+                    jobTitle: htmlResponse.getAttribute("data-indeed-apply-jobTitle"),
+                    jobLocation: htmlResponse.getAttribute("data-indeed-apply-jobLocation"),
+                    jobCompanyName: htmlResponse.getAttribute("data-indeed-apply-jobCompanyName"),
+                    jobURL: htmlResponse.getAttribute("data-indeed-apply-jobUrl")
+                })
 
-        })
-        .then(indeedJob => res.json(indeedJob))
+            })
+            .then(indeedJob => res.json(indeedJob))
+    }
+    else if (req.body.site === "LinkedIn") {
+        const options = {
+            uri: req.body.URL,
+            headers: {
+                "accept": "text/html",
+                "accept-encoding": "br",
+                "accept-language": "en-US,en;q=0.8,ms;q=0.6",
+                "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
+            }
+
+        }
+        rp(options)
+            .then(resp => {
+                const jobFields = {
+                    jobTitle: HTMLParser.parse(resp).querySelector(".sub-nav-cta__header").text,
+                    jobLocation: HTMLParser.parse(resp).querySelector(".sub-nav-cta__sub-text-container").firstChild.text,
+                    jobCompanyName: HTMLParser.parse(resp).querySelector(".sub-nav-cta__sub-text-container").lastChild.text,
+                    jobURL: req.body.URL
+                }
+                               
+                return (jobFields)
+                
+            })
+            .then(linkedinJob => res.json(linkedinJob))
+            .catch(err => console.log(err))
+
+
+    }
 
 }
 
@@ -19,17 +48,6 @@ module.exports = {
     handleScraper: handleScraper
 }
 
-//let url = "https://ca.indeed.com/rc/clk?jk=af8e98d91ec68409&amp;from=vj&amp;ia=1&amp;sjdu=vQIlM60yK_PwYat7ToXhk_BtMgCfoRREjwFTpnTZpxDr2nMIVM2LzYtTq8PoTuWCyVgYsB-CrVACzuliRquR0KdVsNzT9tVrFUobQiL-3go&amp;spon=1&amp;adid=347462548&amp;pos=twoPaneCopyLink"
-//Works
-
 
 //let url = "https://ca.indeed.com/viewjob?jk=e7fd7b4781a9ebc2&tk=1e9eebcgc4ouu800&from=serp&vjs=3&advn=6114451463185775&adid=196595498&sjdu=XuMSTO2lVbIQBVQc5QpEYbP5n_-s08hrxMD-dtpZk1kZUApBFh4bKDvm7LdYQCE42rEk1z1jvfUYyBKDz740wBUGplM10pGaORioW3mZaM2CcPUpqIpeYedZh5I4mDxMcNBoGZC3UaGLRyjmtX1Ys8Y5n05Y-_DiB1f4S4gQMbI"
 //Doesnt work for out of indeed apply, stored in html3 file
-
-//let url = "https://ca.indeed.com/viewjob?cmp=VCOMP-Inc.&t=Web+Developer&jk=b9dde5ed9d7d8b62&sjdu=vQIlM60yK_PwYat7ToXhk9q0RyovIEGBKuRB9fhFNbH1hIAF9nj03DCFVZ9zvEVj4eyQKDIQN1ehdWqXZfv1Hf_urytnNe1ZLdH-k_8vzLLYjjmwitoVms14qSNwihpxc5gYht6mGO1DHpkAgEn3KQ&tk=1e9eegvcl4191800&adid=317358990&pub=4a1b367933fd867b19b072952f68dceb&vjs=3"
-//Works
-
-
-//Job Location, City. 
-//Role
-//Org Name, make sure all fields have the same format
