11d63493edfdf01893af32f616b322b6846d2d26
Ahsan Ullah <ahsanullah.au@gmail.com>
2020-06-19 16:03:45 -0400

    Created methods for retrieval, addition, modification, deletion of doc info from DB

---
+++ src/docAccess.js
@@ -0,0 +1,72 @@
+const handleAddDoc = (req, res, db) => {
+    if (!req.body.userID || !req.body.fileName || !req.body.fileURL) {
+        return res.status(400).json('Missing Info');
+    }
+
+
+    return db('docs')
+        .returning('*')
+        .insert({
+            userid: req.body.userID,
+            filename: req.body.fileName,
+            fileurl: req.body.fileURL,
+        })
+        .then((response) => res.json(response[0]))
+        .catch((err) => res.status(400).json(err));
+};
+
+const handleGetDocs = (req, res, db) => {
+    const { userID } = req.params;
+    if (!userID) {
+        return res.status(400).json('Missing Info');
+    }
+
+    return db('docs')
+        .select({
+
+            docID: 'docid',
+            fileName: 'filename',
+            fileURL: 'fileurl',
+
+        })
+        .where('userid', parseInt(userID, 10))
+        .then((response) => res.json(response))
+        .catch((err) => res.status(400).json(err));
+};
+
+const handleUpdateDoc = (req, res, db) => {
+    if (!req.body.docID || !req.body.fileName || !req.body.fileURL) {
+        return res.status(400).json('Missing Info');
+    }
+
+    return db('docs')
+        .returning('*')
+        .where('docid', req.body.docID)
+        .update({
+            filename: req.body.fileName,
+            fileurl: req.body.fileURL,
+        })
+        .then((response) => res.json(response[0]))
+        .catch((err) => res.status(400).json(err));
+};
+
+const handleDeleteDoc = (req, res, db) => {
+    if (!req.body.docID) {
+        return res.status(400).json('Missing Info');
+    }
+
+    return db('docs')
+        .returning('*')
+        .where('docid', req.body.docID)
+        .del()
+        .then((response) => res.json(response[0]))
+        .catch((err) => res.status(400).json(err));
+};
+
+
+module.exports = {
+    handleAddDoc,
+    handleGetDocs,
+    handleUpdateDoc,
+    handleDeleteDoc,
+};
