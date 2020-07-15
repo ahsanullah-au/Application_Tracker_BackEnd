44da86dad6dab6e331761dc0795a07dc9cae2cce
Ahsan Ullah <ahsanullah.au@gmail.com>
2020-07-15 19:15:13 -0400

    Adding port for heroku

--- src/server.js
+++ src/server.js
@@ -88,6 +88,6 @@ app.put('/docLink', (req, res) => { docAccess.handleUpdateLinkDoc(req, res, db);
 
 
 
-app.listen(3001, () => {
-  console.log('app is running on port 3001');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT||3001}`);
 });
